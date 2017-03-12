44a4da384563f0908be877df22ee9bfe85ff9901
Noah Isaacson <noah@nisaacson.com>
2017-03-11 21:19:25 -0700

    Adds missing quotes

--- status.sh
+++ status.sh
@@ -5,5 +5,5 @@ echo "INSTALLATION_ID: \"$INSTALLATION_ID\""
 PAYLOAD="{\"state\": \"success\", \"id\": \"$CODEBUILD_BUILD_ID\", \"region\": \"$AWS_REGION\"}"
 
 echo "payload is: $PAYLOAD"
-curl -XPOST --data $PAYLOAD "https://05veg8ph09.execute-api.us-west-2.amazonaws.com/dev/status"
+curl -XPOST --data "$PAYLOAD" "https://05veg8ph09.execute-api.us-west-2.amazonaws.com/dev/status"
 echo "build status complete"
