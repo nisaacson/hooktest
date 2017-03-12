c53627d955339d1ebf693267963cdbfb1aa4e1ec
Noah Isaacson <noah@nisaacson.com>
2017-03-11 21:16:42 -0700

    Fixes payload

--- status.sh
+++ status.sh
@@ -2,21 +2,8 @@ env
 echo "HASH: \"$HASH\""
 echo "CODEBUILD_BUILD_ID: \"$CODEBUILD_BUILD_ID\""
 echo "INSTALLATION_ID: \"$INSTALLATION_ID\""
-cat > payload.json <<EOF
-{
-  "state": "success",
-  "id": "$CODEBUILD_BUILD_ID",
-  "region": "$AWS_REGION"
-}
-EOF
+PAYLOAD="{\"state\": \"success\", \"id\": \"$CODEBUILD_BUILD_ID\", \"region\": \"$AWS_REGION\"}"
 
-echo "payload is:"
-cat payload.json
-echo "post data"
-curl --request POST \
-  --verbose \
-  --url "https://05veg8ph09.execute-api.us-west-2.amazonaws.com/dev/status" \
-  --header 'content-type: application/json' \
-  --data @payload.json
+echo "payload is: $PAYLOAD"
+curl -XPOST --data $PAYLOAD "https://05veg8ph09.execute-api.us-west-2.amazonaws.com/dev/status"
 echo "build status complete"
-rm payload.json
