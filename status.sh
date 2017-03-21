adaec02a1af784be32593ffaa4ea167bd304fd25
Noah Isaacson <noah@nisaacson.com>
2017-03-21 10:51:56 -0600

    update script

--- status.sh
+++ status.sh
@@ -4,10 +4,8 @@ set -e
 echo "HASH: \"$HASH\""
 echo "CODEBUILD_BUILD_ID: \"$CODEBUILD_BUILD_ID\""
 echo "INSTALLATION_ID: \"$INSTALLATION_ID\""
+PAYLOAD="{\"state\": \"success\", \"id\": \"$CODEBUILD_BUILD_ID\", \"region\": \"$AWS_REGION\"}"
 
-curl \
-  -XPOST \
-  "https://codereview.dev.api.nisaacson.com/status" -v \
-  --data {"state": "success", "id": "$CODEBUILD_BUILD_ID", "region": "$AWS_REGION"}
-
+echo "payload is: $PAYLOAD"
+curl -XPOST --data "$PAYLOAD" "https://codereview.dev.api.nisaacson.com/status" -v
 echo "build status complete"
