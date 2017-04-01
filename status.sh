081ec9de8e721957d6e31fd582971053c3823b57
Noah Isaacson <noah@nisaacson.com>
2017-04-01 17:06:25 -0600

    Fixes status script

--- status.sh
+++ status.sh
@@ -7,5 +7,5 @@ echo "INSTALLATION_ID: \"$INSTALLATION_ID\""
 PAYLOAD="{\"state\": \"success\", \"id\": \"$CODEBUILD_BUILD_ID\", \"region\": \"$AWS_REGION\"}"
 
 echo "payload is: $PAYLOAD"
-curl -XPOST --data "$PAYLOAD" "https://codereview.dev.api.nisaacson.com/status" -v
+curl -XPOST --data "$PAYLOAD" "https://api.deploytoproduction.com/status" -v
 echo "build status complete"
